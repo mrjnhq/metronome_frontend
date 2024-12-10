57b9ac56e066f0ab04ec0ee8f53fe224b2a63f08
Sabir Islam Khan <34248839+BeLikeSohan@users.noreply.github.com>
2024-12-10 21:28:07 +0600

    Vite Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import LoginSignup from './LoginSignup/LoginSignup'
+
+function App() {
+  return (
+    <LoginSignup></LoginSignup>
+  )
+}
+
+export default App
